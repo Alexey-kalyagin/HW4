487bd1f24cb2e1a11451d680cfcfe048f5a34b63
Alex <alexey.kalyagin@ya.ru>
2022-08-26 18:23:58 +0300

    задача29

---
+++ Задача29/Program.cs
@@ -0,0 +1,23 @@
+﻿ // Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+
+
+
+    int [] array = new int [5];
+    array [0] = 1;
+    array [1] = 2;
+    array [2] = 5;
+    array [3] = 7;
+    array [4] = 19;
+
+static void WrireArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}     
+
+    WrireArray(array);
+
+
